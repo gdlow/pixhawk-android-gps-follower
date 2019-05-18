36d063b6e436a40e3c1ef72bcfd23c1bee8c8aa2
Gerald Low <bcl15@ic.ac.uk>
2019-05-18 18:47:13 +0100

    Refactored with header stamps. To test

--- src/follower_node.cpp
+++ src/follower_node.cpp
@@ -1,6 +1,7 @@
 /**
  * @file follower_node.cpp
- * @brief Offboard control example node to follow an android GPS waypoint
+ * @brief Offboard control example node to follow an android GPS waypoint. 
+ * Fixed height travel (needs to be edited for obstacle avoidance)
  */
 
 #include <ros/ros.h>
@@ -8,12 +9,18 @@
 #include <sensor_msgs/NavSatFix.h>
 #include <mavros_msgs/State.h>
 
+// callback functions
 mavros_msgs::State current_state;
 void state_cb(const mavros_msgs::State::ConstPtr& msg){
     current_state = *msg;
 }
 
 mavros_msgs::GlobalPositionTarget target_pos;
+// Fixed altitude throughout
+void get_target_alt(const sensor_msgs::NavSatFix::ConstPtr& msg) {
+    target_pos.altitude = msg->altitude;
+}
+
 void get_target_pos(const sensor_msgs::NavSatFix::ConstPtr& msg) {
 	target_pos.latitude = msg->latitude;
 	target_pos.longitude = msg->longitude;
@@ -24,38 +31,49 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "follower_node");
     ros::NodeHandle nh;
 
-    // Initialise sub/pub and service clients
     ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>
             ("mavros/state", 10, state_cb);
-    // Android target position
+    ros::Subscriber curr_pos_sub = nh.subscribe<sensor_msgs::NavSatFix>
+            ("mavros/global_position/global", 1, get_target_alt);
     ros::Subscriber target_pos_sub = nh.subscribe<sensor_msgs::NavSatFix>
     		("/android/fix", 10, get_target_pos);
     ros::Publisher target_pos_pub = nh.advertise<mavros_msgs::GlobalPositionTarget>
-            ("mavros/setpoint_raw/global", 10);
+            ("mavros/setpoint_position/global", 10);
 
     //the setpoint publishing rate MUST be faster than 2Hz
-    ros::Rate rate(20.0);
+    ros::Rate rate(10.0);
 
     // wait for FCU connection
     while(ros::ok() && !current_state.connected){
+        ROS_INFO_ONCE("Waiting for FCU connection...");
         ros::spinOnce();
         rate.sleep();
     }
+    ROS_INFO("FCU connected");
+
+    // wait for position information
+    while (ros::ok() && !global_position_received) {
+        ROS_INFO_ONCE("Waiting for GPS signal...");
+        ros::spinOnce();
+        rate.sleep();
+    }
+    ROS_INFO("GPS position received");
 
     //send a few setpoints before starting
-    for(int i = 10; ros::ok() && i > 0; --i){
+    for(int i = 20; ros::ok() && i > 0; --i){
+        target_pos.header.stamp = ros::Time::now();
     	target_pos_pub.publish(target_pos);
-    	ROS_INFO("Publishing latitude: [%f]", target_pos.latitude);
-    	ROS_INFO("Publishing longitude: [%f]", target_pos.longitude);
         ros::spinOnce();
         rate.sleep();
     }
 
     while(ros::ok()){
-
+        target_pos.header.stamp = ros::Time::now();
         target_pos_pub.publish(target_pos);
 
         ros::spinOnce();
+        ROS_INFO_THROTTLE(1, "Target at latitude %f", target_pos.latitude);
+        ROS_INFO_THROTTLE(1, "Target at longitude %f", target_pos.longitude);
         rate.sleep();
     }
 
